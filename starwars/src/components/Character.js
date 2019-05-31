802f3c2a483b2f6718e878b1b9e763040fdc17b4
anthonypiazza <piazza.anthony.n@gmail.com>
2019-05-31 11:25:18 -0500

    MVP Complete

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Character = props => {
+    return (
+        <div className="character">
+            <h1>{props.char.name}</h1>
+            <p>Gender: {props.char.gender}</p>
+            <p>Mass: {props.char.mass}</p>
+            <p>Eye Color: {props.char.eye_color}</p>
+        </div>
+    )
+}
+
+export default Character;
